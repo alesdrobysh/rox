4fb06740034d3a5d8faf99c175ae3cb2b7ac1908
Ales Drobysh <drobysh.ales@gmail.com>
2025-01-20 19:05:35 +0100

    bytecode chunks

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,29 @@
+type Value = f64;
+enum OpCode {
+    Return,
+    Constant(Value),
+}
+
+struct Chunk {
+    op_code: OpCode,
+    line: u32,
+}
+
+fn print_chunk(chunk: Chunk) {
+    match chunk.op_code {
+        OpCode::Return => println!("{:4} RETURN", chunk.line),
+        OpCode::Constant(value) => println!("{:4} CONSTANT {}", chunk.line, value),
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    print_chunk(Chunk {
+        op_code: OpCode::Return,
+        line: 1,
+    });
+
+    print_chunk(Chunk {
+        op_code: OpCode::Constant(1.0),
+        line: 2,
+    });
 }
